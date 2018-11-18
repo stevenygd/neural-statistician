802e2e50906af1cbee7c9a9798f21bec9ffd3b2e
Guandao Yang <gy46@cornell.edu>
2018-11-17 22:06:46 -0500

    update

---
+++ download.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+cd data
+
+wget https://github.com/yburda/iwae/blob/master/datasets/OMNIGLOT/chardata.mat
+wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
